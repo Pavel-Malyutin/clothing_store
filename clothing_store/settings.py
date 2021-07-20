01b8d70ef85bd4c15c384224b89e652182c4675a
User <wer@qer.3>
2021-07-20 13:46:33 +0300

    added products

--- clothing_store/settings.py
+++ clothing_store/settings.py
@@ -27,6 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_app',
+    'category',
+    'accounts',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'clothing_store.wsgi.application'
+
+AUTH_USER_MODEL = 'accounts.Account'
 
 
 # Database
@@ -118,3 +123,6 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from store.models import Product
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'index.html')
+    products_for_main_page = Product.objects.all().filter(is_available=True)[:20]
+    products = Product.objects.all().filter(is_available=True)
+    context = {
+        'products': products_for_main_page,
+    }
+    return render(request, 'index.html', context)
+
+
